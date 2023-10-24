de7b61af7b16e36e48fcab571322136b1b00f66a
sso <93847254+ssoq@users.noreply.github.com>
2023-10-24 16:45:15 +0100

    added timescale to loadlevel function to allow restart

--- SpookyGJ23/Assets/Scripts/UI/MainMenuUI.cs
+++ SpookyGJ23/Assets/Scripts/UI/MainMenuUI.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class MainMenuUI : MonoBehaviour
 {
     public void LoadLevel() 
-    { 
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Level1");
     }
 }
